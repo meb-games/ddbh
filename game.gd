6a9aecf82179dd7689b0ed654e0b1fca33c5091b
EvePanima <106990972+EvePanima@users.noreply.github.com>
2026-01-06 00:51:27 -0500

    Add files via upload
    added a health bar and made minor bug fix to movement

--- game.gd
+++ game.gd
@@ -4,6 +4,8 @@ extends Node2D
 
 func _ready() -> void:
 	var mage = MAGE_SCENE.instantiate()
+	# is trying to call update healthbar ui but dont know how to make function global and as stated below too tired to proberly write
+	$HealthBar.value = 10 #NEEDS TO BE CHANGED IM JUST TOO TIRED TO PROBERELY WRITE
 	mage.position.x += 100
 	mage.position.y += 100
 	add_child(mage)
