1e63208b268a4c3b3769b0e805369f3d649be43a
Matalya <matalya8@gmail.com>
2026-04-08 13:00:49 -0300

    set up portals for rooms of level 1

--- game/map_elements/portal.gd
+++ game/map_elements/portal.gd
@@ -3,22 +3,22 @@ extends Area2D
 @export var out: Node2D
 
 func _init() -> void:
-	self.body_entered.connect(func(explorer):
-		explorer.collision_layer = explorer.collision_layer & (~16)
-		
-		var viewport = get_viewport()
-		viewport.get_camera_2d().position = self.out.get_parent().position + (viewport.get_visible_rect().size / 2)
-		$/root/Game/UI.position = self.out.get_parent().position
-		explorer.position = self.out.position + self.out.get_parent().position
-		self.out.get_parent().get_parent().currentRoom = self.out.get_parent()
-		
-		await get_tree().create_timer(2.0).timeout
-		if explorer != null:
-			explorer.collision_layer = explorer.collision_layer | 16
-	)
+    self.body_entered.connect(func(explorer):
+        explorer.collision_layer = explorer.collision_layer & (~16)
+        
+        var viewport = get_viewport()
+        viewport.get_camera_2d().position = self.out.get_parent().position + (viewport.get_visible_rect().size / 2)
+        $/root/Game/UI.position = self.out.get_parent().position
+        explorer.position = self.out.position + self.out.get_parent().position
+        self.out.get_parent().get_parent().currentRoom = self.out.get_parent()
+        
+        await get_tree().create_timer(2.0).timeout
+        if explorer != null:
+            explorer.collision_layer = explorer.collision_layer | 16
+    )
 
 func _ready() -> void:
-	if self.get_parent() is not Room:
-		printerr("Portals must be children of a room")
-	if self.out.get_parent() is not Room:
-		printerr("Portal outputs must be children of a room")
+    if self.get_parent() is not Room:
+        printerr("Portals must be children of a room")
+    if self.out.get_parent() is not Room:
+        printerr("Portal outputs must be children of a room")
--- game/room.gd
+++ game/room.gd
@@ -5,14 +5,14 @@ var waves: Array[Wave] = []
 var current_wave: int = 0
 
 func currentWave() -> Wave:
-	return self.waves[self.current_wave]
+    return self.waves[self.current_wave]
 
 
 func _ready() -> void:
-	if self.get_parent() is not Level:
-		printerr("Rooms must be children of a level")
+    if self.get_parent() is not Level:
+        printerr("Rooms must be children of a level")
 
 
 func _on_explorer_exit() -> void:
-	for enemy in self.currentWave().get_children():
-		enemy._stop()
+    for enemy in self.currentWave().get_children():
+        enemy._stop()
