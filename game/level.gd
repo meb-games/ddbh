5dad369eb4f7c7ffee3f935275580a9963b3a7e1
BrightShard <brightshard@brightshard.dev>
2026-04-08 12:07:45 -0400

    merge

--- game/level.gd
+++ game/level.gd
@@ -1,3 +1,4 @@
+@tool
 extends Node2D
 class_name Level
 
@@ -5,10 +6,24 @@ var currentRoom: Room
 @export var startPosition: Node2D
 
 func _ready() -> void:
-	if self.currentRoom == null:
-		self.currentRoom = self.get_child(0)
-	$/root/Game/Explorer.position = self.startPosition.position
+	if Engine.is_editor_hint():
+		pass
+		# bad experiment to automatically draw borders around rooms in the level
+		#for room in self.get_children():
+			#print("meow" + room.name)
+			#var frame = Panel.new()
+			#var style = StyleBoxFlat.new()
+			#style.set_border_width_all(1)
+			#style.border_color = Color.AQUAMARINE
+			#frame["theme_override_styles/panel"] = style
+			#room.add_child(frame)
+			#frame.owner = get_tree().edited_scene_root
+	else:
+		if self.currentRoom == null:
+			self.currentRoom = self.get_child(0)
+		$/root/Game/Explorer.position = self.startPosition.position
 
 func _physics_process(delta: float) -> void:
-	for enemy in self.currentRoom.currentWave().get_children():
-		enemy._tick()
+	if !Engine.is_editor_hint():
+		for enemy in self.currentRoom.currentWave().get_children():
+			enemy._tick()
