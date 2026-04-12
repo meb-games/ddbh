a69a2bdf704debbeb90064d902a60ba088c5d654
BrightShard <brightshard@brightshard.dev>
2026-04-12 14:13:05 -0400

    maybe got waves working

--- game/room.gd
+++ game/room.gd
@@ -5,13 +5,19 @@ var waves: Array[Wave] = []
 var current_wave: int = 0
 
 func currentWave() -> Wave:
-    return self.waves[self.current_wave]
+	return self.waves[self.current_wave]
 
 
 func _ready() -> void:
-    if self.get_parent() is not Level:
-        printerr("Rooms must be children of a level")
-        
+	if self.get_parent() is not Level:
+		printerr("Rooms must be children of a level")
+	self.currentWave().start()
+		
 func _on_explorer_exit() -> void:
-    for enemy in self.currentWave().get_children():
-        enemy._stop()
+	for enemy in self.currentWave().get_children():
+		enemy._stop()
+
+func _on_enemy_die() -> void:
+	if self.currentWave().get_child_count() == 0:
+		self.current_wave += 1
+		self.currentWave().start()
--- game/wave.gd
+++ game/wave.gd
@@ -1,5 +1,15 @@
 extends Node2D
 class_name Wave
 
+var og_positions = []
+
 func _ready() -> void:
 	self.get_parent().waves.append(self)
+	for child in self.get_children():
+		og_positions.append(child.position)
+		child.global_position = Vector2(-100, 100)
+
+func start():
+	for idx in range(0, self.get_child_count()):
+		self.get_child(idx).position = og_positions[idx]
+		og_positions = null
