872ba194e9fa7d9b5db4c679b4f4d2d8ed2c72f1
BrightShard <brightshard@brightshard.dev>
2026-05-29 17:21:21 -0400

    fix room alignments for level 3, add dumb way to test new levels

--- game.gd
+++ game.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
 var tick := 0
+@export var level_to_load: int = 1
 
-func load_level(level_name: String) -> void:
-	$/root/Game.add_child(load("res://game/levels/%s.tscn" % level_name).instantiate())
+func load_level(level: int) -> void:
+	$/root/Game.add_child(load("res://game/levels/%s.tscn" % level).instantiate())
 
 func _ready() -> void:
-	self.load_level("1")
+	self.load_level(level_to_load)
 
 func _process(_delta: float) -> void:
 	self.tick += 1
