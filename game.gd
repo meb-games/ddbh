f35a3168ff51bcc9ab7c8047e6b9e8919746caf5
Matalya <matalya8@gmail.com>
2026-07-04 16:14:22 -0300

    set health to enemies

--- game.gd
+++ game.gd
@@ -4,14 +4,14 @@ var tick := 0
 @export var level_to_load: int = 1
 
 func load_level(levelId: int) -> void:
-	if self.has_node("Level"):
-		$Level.queue_free()
-	var level = load("res://game/levels/%s.tscn" % levelId).instantiate()
-	level.name = "Level"
-	$/root/Game.add_child(level)
+    if self.has_node("Level"):
+        $Level.queue_free()
+    var level = load("res://game/levels/%s.tscn" % levelId).instantiate()
+    level.name = "Level"
+    $/root/Game.add_child(level)
 
 func _ready() -> void:
-	self.load_level(level_to_load)
+    self.load_level(level_to_load)
 
 func _process(_delta: float) -> void:
-	self.tick += 1
+    self.tick += 1
--- game/room.gd
+++ game/room.gd
@@ -8,49 +8,49 @@ var current_wave: int = 0
 var enemy_died := false
 
 func currentWave() -> Wave:
-	if self.current_wave == len(self.waves):
-		return null
-	return self.waves[self.current_wave]
+    if self.current_wave == len(self.waves):
+        return null
+    return self.waves[self.current_wave]
 
 func level() -> Level:
-	return self.get_parent()
-	
+    return self.get_parent()
+    
 func activate():
-	var viewport = get_viewport()
-	viewport.get_camera_2d().position = self.position + (viewport.get_visible_rect().size / 2)
-	$/root/Game/UI.position = self.position
-	self.level().currentRoom = self
+    var viewport = get_viewport()
+    viewport.get_camera_2d().position = self.position + (viewport.get_visible_rect().size / 2)
+    $/root/Game/UI.position = self.position
+    self.level().currentRoom = self
 
 
 
 func _ready() -> void:
-	if self.get_parent() is not Level:
-		printerr("Rooms must be children of a level")
-	if self.has_node("PortalOut"):
-		$PortalOut.disable()
-	self.currentWave().start()
-	
+    if self.get_parent() is not Level:
+        printerr("Rooms must be children of a level")
+    if self.has_node("PortalOut"):
+        $PortalOut.disable()
+    self.currentWave().start()
+    
 func _process(_delta: float) -> void:
-	if self.enemy_died:
-		self.enemy_died = false
-		if self.currentWave().get_child_count() == 0:
-			self.current_wave += 1
-			if self.current_wave == len(self.waves):
-				self._on_all_waves_defeated()
-			else:
-				self.currentWave().start()
+    if self.enemy_died:
+        self.enemy_died = false
+        if self.currentWave().get_child_count() == 0:
+            self.current_wave += 1
+            if self.current_wave == len(self.waves):
+                self._on_all_waves_defeated()
+            else:
+                self.currentWave().start()
 
 func _on_explorer_exit() -> void:
-	var wave := self.currentWave()
-	if wave != null:
-		for enemy in wave.get_children():
-			enemy._stop()
+    var wave := self.currentWave()
+    if wave != null:
+        for enemy in wave.get_children():
+            enemy._stop()
 
 func _on_enemy_die() -> void:
-	self.enemy_died = true
+    self.enemy_died = true
 
 func _on_all_waves_defeated():
-	if self.has_node("PortalOut"):
-		$PortalOut.enable()
-	elif self.final_room:
-		$/root/Game/UI/Victory.visible = true
+    if self.has_node("PortalOut"):
+        $PortalOut.enable()
+    elif self.final_room:
+        $/root/Game/UI/Victory.visible = true
